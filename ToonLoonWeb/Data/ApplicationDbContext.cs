edd0428c696a0cf1c27dfed3325a9f1957718c6d
philip <philipjohn.segarra@gmail.com>
2024-05-16 18:01:22 +0800

    sixth commit

--- ToonLoonWeb/Data/ApplicationDbContext.cs
+++ ToonLoonWeb/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ToonLoonWeb.Client.Models;
+using ToonLoonWeb.Models;
 
 namespace ToonLoonWeb.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        DbSet<Favorites> Favorites { get; set; }
     }
 }
