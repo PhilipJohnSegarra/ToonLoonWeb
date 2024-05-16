26251d4d00179e97796f3a11f1d7a51c90645715
philip <philipjohn.segarra@gmail.com>
2024-05-16 18:01:22 +0800

    sixth commit

---
+++ ToonLoonWeb.Client/Models/Favorites.cs
@@ -0,0 +1,9 @@
+﻿namespace ToonLoonWeb.Client.Models
+{
+    public class Favorites
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string? MangaID { get; set; }
+    }
+}
--- ToonLoonWeb/Data/Migrations/20240516090900_m.Designer.cs
+++ ToonLoonWeb/Data/Migrations/20240516090900_m.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToonLoonWeb.Data;
 
@@ -11,9 +12,11 @@ using ToonLoonWeb.Data;
 namespace ToonLoonWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240516090900_m")]
+    partial class m
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
